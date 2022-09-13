cd4a6db25e9f416ec8070fe4b3437e6360e1c602
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2022-09-13 16:13:39 +0100

    Update popup implemented

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,6 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'package:passy/passy_data/common.dart';
 import 'package:passy/passy_data/passy_data.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
     RouteObserver<ModalRoute<void>>();
 late PassyData data;
+
+Future<String> getLatestVersion() async {
+  try {
+    String _version = (jsonDecode(await http.read(
+      Uri.https('api.github.com', 'repositories/469494355/releases/latest'),
+    ))[0]!['tag_name'] as String);
+    if (_version[0] == 'v') {
+      _version = _version.substring(1);
+    }
+    return _version;
+  } catch (_) {
+    return passyVersion;
+  }
+}
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:passy/passy_data/common.dart';
+import 'package:passy/passy_flutter/passy_flutter.dart';
 import 'package:passy/screens/add_account_screen.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:universal_io/io.dart';
 import 'package:passy/common/common.dart';
 import 'package:passy/passy_data/passy_data.dart';
 import 'package:passy/common/assets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'login_screen.dart';
 
@@ -16,16 +20,42 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future<void> showUpdateDialog() {
+      return showDialog<void>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: const Text('New version available'),
+          content: ThreeWidgetButton(
+            left: Padding(
+              padding: const EdgeInsets.only(right: 30),
+              child: SvgPicture.asset(
+                'assets/images/github_icon.svg',
+                width: 26,
+                color: PassyTheme.lightContentColor,
+              ),
+            ),
+            center: const Text('Download'),
+            right: const Icon(Icons.arrow_forward_ios_rounded),
+            onPressed: () => launchUrl(
+              Uri.https('github.com', '/GlitterWare/Passy/releases/latest'),
+            ),
+          ),
+        ),
+      );
+    }
+
     Future<void> _load() async {
       data = PassyData((await getApplicationDocumentsDirectory()).path +
           Platform.pathSeparator +
           'Passy');
       loaded = true;
+      String _version = await getLatestVersion();
       if (data.noAccounts) {
         Navigator.pushReplacementNamed(context, AddAccountScreen.routeName);
         return;
       }
       Navigator.pushReplacementNamed(context, LoginScreen.routeName);
+      if (_version != passyVersion) showUpdateDialog();
     }
 
     if (!loaded) {
