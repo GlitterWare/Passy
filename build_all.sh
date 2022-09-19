ca34ec9b968e8f84f17f5c942a1a22fd19190d9b
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2022-09-19 17:00:19 +0100

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -33,6 +33,6 @@ echo 'v'$version | ./appimage/appimage_builder
 echo ''
 echo 'INFO:Finished building version '$version
 echo 'Results:'
-echo '- Android Apk - ./build/app/outputs/flutter-apk/app-release.apk'
-echo '- Linux x86_64 Bundle - ./build/linux/x64/release/bundle'
-echo '- Linux x86_64 AppImage - ./build/appimage/Passy-v'$version'-x86_64.AppImage'
+echo '- Android Apk - '$PWD'/build/app/outputs/flutter-apk/app-release.apk'
+echo '- Linux x86_64 Bundle - '$PWD'/build/linux/x64/release/bundle'
+echo '- Linux x86_64 AppImage - '$PWD'/build/appimage/Passy-v'$version'-x86_64.AppImage'
