8306ff9d30f4ed2c2b5dc13ea99eda6cdb4e9d36
gleam <101527589+GleammerRay@users.noreply.github.com>
2022-10-31 18:42:20 +0000

    Linux building improvements

--- build_all.sh
+++ build_all.sh
@@ -7,17 +7,18 @@ user_interrupt(){
 trap user_interrupt SIGINT
 trap user_interrupt SIGTSTP
 
+read -p "? Version [Eg: 1.0.0]: " appVersion
+
 echo 'INFO:Building APK'
 flutter build apk
 echo 'INFO:Building Linux Bundle'
 flutter build linux
 cp ./linux_meta/* './build/linux/x64/release/bundle'
-cp '../assets/images/logo_purple.svg' './build/linux/x64/release/bundle'
+cp './assets/images/logo_purple.svg' './build/linux/x64/release/bundle/com.glitterware.passy.svg'
 echo 'INFO:Building Linux AppImage'
-echo 'v'$version | ./appimage/appimage_builder
+echo 'v'$appVersion | ./appimage/appimage_builder
 echo ''
-echo 'INFO:Finished building version '$version
 echo 'Results:'
 echo '- Android Apk - '$PWD'/build/app/outputs/flutter-apk/app-release.apk'
 echo '- Linux x86_64 Bundle - '$PWD'/build/linux/x64/release/bundle'
-echo '- Linux x86_64 AppImage - '$PWD'/build/appimage/Passy-v'$version'-x86_64.AppImage'
+echo '- Linux x86_64 AppImage - '$PWD'/build/appimage/Passy-v'$appVersion'-x86_64.AppImage'
