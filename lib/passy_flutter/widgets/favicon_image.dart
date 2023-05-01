6bf639097d19c2a089b15426988fa4f696120c32
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-01 02:46:18 +0100

    Scraping favicons instead of using an online API

--- lib/passy_flutter/widgets/favicon_image.dart
+++ lib/passy_flutter/widgets/favicon_image.dart
@@ -1,7 +1,19 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:passy/common/assets.dart';
+import 'package:favicon/favicon.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
+
+bool _isFaviconManagerStarted = false;
+Completer _faviconManagerCompleter = Completer();
+Map<String, dynamic> _favicons = {};
+bool _saveRequested = false;
 
 class FavIconImage extends StatelessWidget {
   final String address;
@@ -15,21 +27,78 @@ class FavIconImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FileInfo? fileInfo;
+    if (!_isFaviconManagerStarted) {
+      _isFaviconManagerStarted = true;
+      Future(() async {
+        fileInfo =
+            await DefaultCacheManager().getFileFromCache('passyfavicons');
+        File file;
+        if (fileInfo == null) {
+          file = await DefaultCacheManager().putFile(
+              'passyfavicons',
+              Uint8List.fromList(
+                  utf8.encode(jsonEncode({'favicons': _favicons}))));
+          file.writeAsStringSync(jsonEncode({'favicons': _favicons}));
+        } else {
+          file = fileInfo!.file;
+          Map<String, dynamic> contents = jsonDecode(file.readAsStringSync());
+          dynamic favicons = contents['favicons'];
+          if (favicons is! Map<String, dynamic>) {
+            favicons = {};
+            contents['favicons'] = favicons;
+            file.writeAsStringSync(jsonEncode(contents));
+          }
+          _favicons = favicons;
+        }
+        _faviconManagerCompleter.complete();
+        Future<void> _faviconManager() async {
+          if (_saveRequested) {
+            file.writeAsStringSync(jsonEncode({'favicons': _favicons}));
+          }
+          Future.delayed(const Duration(seconds: 5), _faviconManager);
+        }
+
+        _faviconManager();
+      });
+    }
     String _url = address;
     SvgPicture _placeholder = SvgPicture.asset(
       logoCircleSvg,
       color: Colors.white,
       width: width,
     );
-    _url = _url.replaceFirst(RegExp('https://|http://'), '');
-    String _request = 'https://api.faviconkit.com/$_url/256';
-
-    return CachedNetworkImage(
-      imageUrl: _request,
-      placeholder: (context, _url) => _placeholder,
-      errorWidget: (ctx, obj, s) => _placeholder,
-      width: width,
-      fit: BoxFit.fill,
+    _url = 'http://' + _url.replaceFirst(RegExp('https://|http://'), '');
+    return FutureBuilder(
+      future: Future(() async {
+        await _faviconManagerCompleter.future;
+        print(_favicons);
+        print(_url);
+        dynamic _imageURL = _favicons[_url];
+        if (_imageURL is String) return Favicon(_imageURL);
+        Favicon? icon = await FaviconFinder.getBest(_url,
+            suffixes: ['png', 'jpg', 'jpeg', 'ico']);
+        print('bad');
+        if (icon == null) return null;
+        _favicons[_url] = icon.url;
+        _saveRequested = true;
+        return icon;
+      }),
+      builder: (BuildContext context, AsyncSnapshot<Favicon?> snapshot) {
+        Favicon? favicon = snapshot.data;
+        if (favicon == null) return _placeholder;
+        return CachedNetworkImage(
+          imageUrl: favicon.url,
+          placeholder: (context, _url) => _placeholder,
+          errorWidget: (ctx, obj, s) {
+            _favicons.remove(_url);
+            _saveRequested = true;
+            return _placeholder;
+          },
+          width: width,
+          fit: BoxFit.fill,
+        );
+      },
     );
   }
 }
