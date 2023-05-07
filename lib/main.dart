86399f4059cdc3d05a81061f3b7df621ab912b6b
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-05-07 07:45:17 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -171,8 +171,7 @@ class Passy extends StatelessWidget {
       supportedLocales: const [
         Locale('en'),
         Locale('it'),
-		Locale('it')
-		//isn't last line supposed to not include a "," ?
+        Locale('tr'),
       ],
     );
   }
