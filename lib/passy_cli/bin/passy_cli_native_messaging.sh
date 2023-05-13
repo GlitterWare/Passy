344161975640afd4c9b3bd3768fd61c1a708c7d1
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-13 17:29:32 +0100

    Update passy_cli_native_messaging.sh

--- lib/passy_cli/bin/passy_cli_native_messaging.sh
+++ lib/passy_cli/bin/passy_cli_native_messaging.sh
@@ -1,1 +1,1 @@
-until eval "$(./passy_cli install temp) native_messaging start" < "/dev/stdin"; do sleep 0.001; done
+until eval "\"$(./passy_cli install temp)\" native_messaging start" < "/dev/stdin"; do sleep 0.001; done
