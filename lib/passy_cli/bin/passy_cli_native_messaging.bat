ab94b0b7a6a91bf28ca10f879687c4d96487f875
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-13 17:54:39 +0100

    Update passy_cli_native_messaging.bat

--- lib/passy_cli/bin/passy_cli_native_messaging.bat
+++ lib/passy_cli/bin/passy_cli_native_messaging.bat
@@ -1,6 +1,6 @@
 @echo off
 SET "PASSY_PATH=%~dp0passy_cli.exe"
 :Start
-FOR /F "tokens=* USEBACKQ" %%F IN (`%%PASSY_PATH%% install temp`) DO (SET PASSY_TEMP_PATH=%%F)
+FOR /F "tokens=* USEBACKQ" %%F IN (`call "%%PASSY_PATH%%" install temp`) DO (SET PASSY_TEMP_PATH=%%F)
 call "%%PASSY_TEMP_PATH%%" native_messaging start
 GOTO:Start
