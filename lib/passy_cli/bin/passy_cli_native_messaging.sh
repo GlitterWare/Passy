617477d42b88e78f62746661dd2777f9a827f827
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-14 15:18:46 +0100

    No more looping in scripts

--- lib/passy_cli/bin/passy_cli_native_messaging.sh
+++ lib/passy_cli/bin/passy_cli_native_messaging.sh
@@ -1,1 +1,1 @@
-until eval "\"$(./passy_cli install temp)\" native_messaging start" < "/dev/stdin"; do sleep 0.001; done
+eval "\"$(./passy_cli install temp)\" native_messaging start" < "/dev/stdin"
