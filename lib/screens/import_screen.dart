4f0af8d23fe6b156531e51757a69f9e3b5b31251
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-16 18:15:26 +0100

    Favicon optimizations + image library updated

--- lib/screens/import_screen.dart
+++ lib/screens/import_screen.dart
@@ -63,7 +63,8 @@ class _ImportScreen extends State<ImportScreen> {
             child: SvgPicture.asset(
               logoCircleSvg,
               width: 30,
-              color: PassyTheme.lightContentColor,
+              colorFilter: const ColorFilter.mode(
+                  PassyTheme.lightContentColor, BlendMode.srcIn),
             ),
           ),
           right: const Icon(Icons.arrow_forward_ios_rounded),
