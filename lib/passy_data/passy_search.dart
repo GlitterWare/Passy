f9f20d08ef8cde0639ac288acfc8a7ae9a7fc441
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-05-16 18:57:14 +0100

    Websites now compared in password search

--- lib/passy_data/passy_search.dart
+++ lib/passy_data/passy_search.dart
@@ -22,6 +22,10 @@ class PassySearch {
             _positiveCount++;
             continue;
           }
+          if (_password.website.toLowerCase().contains(_term)) {
+            _positiveCount++;
+            continue;
+          }
         }
         if (_positiveCount == _terms.length) {
           _found.add(_password);
