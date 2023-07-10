23bb5f84093d0e1efae4ccdd12022b6e0f31495f
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-07-10 11:45:12 +0100

    Version changed

--- lib/passy_data/common.dart
+++ lib/passy_data/common.dart
@@ -6,7 +6,7 @@ import 'package:encrypt/encrypt.dart';
 import 'package:path/path.dart' as path;
 import 'dart:io';
 
-const String passyVersion = '1.5.0';
+const String passyVersion = '1.5.1';
 const String syncVersion = '2.0.0';
 const String accountVersion = '2.2.0';
 
