6ecbe26f91ac9401aea32f9470d0073990f188c1
gleam <101527589+GleammerRay@users.noreply.github.com>
2023-07-31 20:17:41 +0100

    Update argon2_info.dart

--- lib/passy_data/argon2_info.dart
+++ lib/passy_data/argon2_info.dart
@@ -1,11 +1,17 @@
+import 'dart:convert';
+
+import 'package:dargon2_interface/dargon2_interface.dart';
+
 import 'key_derivation_info.dart';
 
 class Argon2Info extends KeyDerivationInfo {
+  Salt salt;
   int parallelism;
   int memory;
   int iterations;
 
   Argon2Info({
+    required this.salt,
     this.parallelism = 4,
     this.memory = 64,
     this.iterations = 2,
@@ -13,6 +19,7 @@ class Argon2Info extends KeyDerivationInfo {
 
   factory Argon2Info.fromJson(Map<String, dynamic> json) {
     return Argon2Info(
+      salt: Salt(base64Decode(json['salt'])),
       parallelism: json['parallelism'] ?? 4,
       memory: json['memory'] ?? 64,
       iterations: json['iterations'] ?? 2,
@@ -21,7 +28,9 @@ class Argon2Info extends KeyDerivationInfo {
 
   @override
   Map<String, dynamic> toJson() {
+    String saltEncoded = base64Encode(salt.bytes);
     return {
+      'salt': saltEncoded,
       'parallelism': parallelism,
       'memory': memory,
       'iterations': iterations,
