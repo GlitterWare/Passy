7f5f87252bce8c9dc05aee0321b338bb2fc44bba
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-09-11 19:39:07 +0100

    Update passy_fs_meta.dart

--- lib/passy_data/passy_fs_meta.dart
+++ lib/passy_data/passy_fs_meta.dart
@@ -2,7 +2,7 @@ import 'package:passy/passy_data/csv_convertable.dart';
 
 import 'entry_event.dart';
 
-abstract class PassyFsMeta with CSVConvertable {
+class PassyFsMeta with CSVConvertable {
   final String key;
   final String name;
   EntryStatus status;
@@ -16,4 +16,14 @@ abstract class PassyFsMeta with CSVConvertable {
   })  : key = key ??
             DateTime.now().toUtc().toIso8601String().replaceAll(':', 'c'),
         entryModified = entryModified ?? DateTime.now().toUtc();
+
+  @override
+  List toCSV() {
+    return [
+      key,
+      name,
+      status.name,
+      entryModified.toIso8601String(),
+    ];
+  }
 }
