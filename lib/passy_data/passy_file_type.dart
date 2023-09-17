4df1c0c4d3e3237ebe81aaf8066ea72739c8e3cc
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-09-17 09:46:27 +0100

    Update passy_file_type.dart

--- lib/passy_data/passy_file_type.dart
+++ lib/passy_data/passy_file_type.dart
@@ -1,5 +1,7 @@
 enum PassyFileType {
   unknown,
+  text,
+  markdown,
   imageRaster,
 }
 
@@ -7,6 +9,10 @@ PassyFileType? passyFileTypeFromName(String name) {
   switch (name) {
     case 'unknown':
       return PassyFileType.unknown;
+    case 'text':
+      return PassyFileType.text;
+    case 'markdown':
+      return PassyFileType.markdown;
     case 'imageRaster':
       return PassyFileType.imageRaster;
   }
