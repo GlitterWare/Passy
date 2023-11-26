ff5c228a24931135503788159d5ba8b89553ed7b
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-11-26 14:35:18 +0000

    Synchronization version changed

--- lib/passy_data/common.dart
+++ lib/passy_data/common.dart
@@ -18,7 +18,7 @@ import 'glare/glare_client.dart';
 import 'key_derivation_type.dart';
 
 const String passyVersion = '1.7.0';
-const String syncVersion = '2.0.2';
+const String syncVersion = '2.1.0';
 const String accountVersion = '2.4.0';
 
 bool isSnap() {
