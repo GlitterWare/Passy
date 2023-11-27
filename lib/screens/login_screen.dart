e70afe6f8ea2d42e0a0a0822e79e0ecb357ce367
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2023-11-27 21:33:49 +0000

    Legacy account login fixed

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:crypto/crypto.dart';
 import 'package:encrypt/encrypt.dart' as enc;
@@ -182,7 +183,8 @@ class _LoginScreen extends State<LoginScreen> {
     data.info.save().whenComplete(() async {
       try {
         enc.Key key = _derivedPassword == null
-            ? enc.Key.fromUtf8(_password + (' ' * (32 - _password.length)))
+            ? enc.Key.fromUtf8(
+                _password + (' ' * (32 - utf8.encode(_password).length)))
             : enc.Key(Uint8List.fromList(_derivedPassword));
         LoadedAccount _account = await data.loadAccount(
             data.info.value.lastUsername,
