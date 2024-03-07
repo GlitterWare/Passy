bb011e1cc4c477184f99739d1dc8702a769a2e75
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-07 23:45:11 +0000

    Update passy_search.dart

--- lib/passy_data/passy_search.dart
+++ lib/passy_data/passy_search.dart
@@ -1,11 +1,15 @@
 import 'package:passy/passy_data/password.dart';
 
 class PassySearch {
-  static List<PasswordMeta> searchPasswords(
-      {required Iterable<PasswordMeta> passwords, required String terms}) {
+  static List<PasswordMeta> searchPasswords({
+    required Iterable<PasswordMeta> passwords,
+    required String terms,
+    List<String> tags = const [],
+  }) {
     final List<PasswordMeta> _found = [];
     final List<String> _terms = terms.trim().toLowerCase().split(' ');
     for (PasswordMeta _password in passwords) {
+      if (_password.tags.length < tags.length) continue;
       {
         bool testPassword(PasswordMeta value) => _password.key == value.key;
 
@@ -13,6 +17,12 @@ class PassySearch {
       }
       {
         int _positiveCount = 0;
+        bool _tagMismatch = false;
+        for (String tag in tags) {
+          if (_password.tags.contains(tag)) continue;
+          _tagMismatch = true;
+        }
+        if (_tagMismatch) continue;
         for (String _term in _terms) {
           if (_password.username.toLowerCase().contains(_term)) {
             _positiveCount++;
