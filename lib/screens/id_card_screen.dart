29f8330f61817aa333821eec98ffd33c2595f3fb
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-10 21:09:28 +0000

    ID Cards tags UI implemented

--- lib/screens/id_card_screen.dart
+++ lib/screens/id_card_screen.dart
@@ -27,12 +27,33 @@ class IDCardScreen extends StatefulWidget {
 class _IDCardScreen extends State<IDCardScreen> {
   final LoadedAccount _account = data.loadedAccount!;
   bool isFavorite = false;
+  IDCard? _idCard;
+  List<String> _tags = [];
+  List<String> _selected = [];
+
+  Future<void> _load() async {
+    List<String> newTags = await _account.idCardsTags;
+    if (mounted) {
+      setState(() {
+        _tags = newTags;
+        _selected = _idCard!.tags.toList();
+        for (String tag in _selected) {
+          if (_tags.contains(tag)) {
+            _tags.remove(tag);
+          }
+        }
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final IDCard _idCard = ModalRoute.of(context)!.settings.arguments as IDCard;
+    if (_idCard == null) {
+      _idCard = ModalRoute.of(context)!.settings.arguments as IDCard;
+      _load();
+    }
     isFavorite =
-        _account.favoriteIDCards[_idCard.key]?.status == EntryStatus.alive;
+        _account.favoriteIDCards[_idCard!.key]?.status == EntryStatus.alive;
 
     void _onRemovePressed() {
       showDialog(
@@ -60,7 +81,7 @@ class _IDCardScreen extends State<IDCardScreen> {
                   ),
                   onPressed: () {
                     Navigator.pushNamed(context, SplashScreen.routeName);
-                    _account.removeIDCard(_idCard.key).whenComplete(() {
+                    _account.removeIDCard(_idCard!.key).whenComplete(() {
                       Navigator.popUntil(context,
                           (r) => r.settings.name == MainScreen.routeName);
                       Navigator.pushNamed(context, IDCardsScreen.routeName);
@@ -76,21 +97,21 @@ class _IDCardScreen extends State<IDCardScreen> {
       Navigator.pushNamed(
         context,
         EditIDCardScreen.routeName,
-        arguments: _idCard,
+        arguments: _idCard!,
       );
     }
 
     return Scaffold(
       appBar: EntryScreenAppBar(
         entryType: EntryType.idCard,
-        entryKey: _idCard.key,
+        entryKey: _idCard!.key,
         title: Center(child: Text(localizations.idCard)),
         onRemovePressed: () => _onRemovePressed(),
         onEditPressed: () => _onEditPressed(),
         isFavorite: isFavorite,
         onFavoritePressed: () async {
           if (isFavorite) {
-            await _account.removeFavoriteIDCard(_idCard.key);
+            await _account.removeFavoriteIDCard(_idCard!.key);
             showSnackBar(context,
                 message: localizations.removedFromFavorites,
                 icon: const Icon(
@@ -98,7 +119,7 @@ class _IDCardScreen extends State<IDCardScreen> {
                   color: PassyTheme.darkContentColor,
                 ));
           } else {
-            await _account.addFavoriteIDCard(_idCard.key);
+            await _account.addFavoriteIDCard(_idCard!.key);
             showSnackBar(context,
                 message: localizations.addedToFavorites,
                 icon: const Icon(
@@ -111,43 +132,85 @@ class _IDCardScreen extends State<IDCardScreen> {
       ),
       body: ListView(
         children: [
-          if (_idCard.attachments.isNotEmpty)
-            AttachmentsListView(files: _idCard.attachments),
-          if (_idCard.nickname != '')
+          Center(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  top: PassyTheme.passyPadding.top / 2,
+                  bottom: PassyTheme.passyPadding.bottom / 2),
+              child: EntryTagList(
+                showAddButton: true,
+                selected: _selected,
+                notSelected: _tags,
+                onAdded: (tag) async {
+                  Navigator.pushNamed(context, SplashScreen.routeName);
+                  _idCard!.tags = _selected.toList();
+                  _idCard!.tags.add(tag);
+                  await _account.setIDCard(_idCard!);
+                  Navigator.popUntil(context,
+                      (route) => route.settings.name == IDCardScreen.routeName);
+                  if (!mounted) return;
+                  setState(() {
+                    _tags.remove(tag);
+                    _selected.add(tag);
+                    _selected.sort();
+                    _idCard!.tags = _selected;
+                  });
+                },
+                onRemoved: (tag) async {
+                  Navigator.pushNamed(context, SplashScreen.routeName);
+                  _idCard!.tags = _selected.toList();
+                  _idCard!.tags.remove(tag);
+                  await _account.setIDCard(_idCard!);
+                  Navigator.popUntil(context,
+                      (route) => route.settings.name == IDCardScreen.routeName);
+                  if (!mounted) return;
+                  setState(() {
+                    _tags.add(tag);
+                    _tags.sort();
+                    _selected.remove(tag);
+                    _idCard!.tags = _selected;
+                  });
+                },
+              ),
+            ),
+          ),
+          if (_idCard!.attachments.isNotEmpty)
+            AttachmentsListView(files: _idCard!.attachments),
+          if (_idCard!.nickname != '')
             PassyPadding(RecordButton(
               title: localizations.nickname,
-              value: _idCard.nickname,
+              value: _idCard!.nickname,
             )),
-          if (_idCard.type != '')
+          if (_idCard!.type != '')
             PassyPadding(RecordButton(
               title: localizations.type,
-              value: _idCard.type,
+              value: _idCard!.type,
             )),
-          if (_idCard.idNumber != '')
+          if (_idCard!.idNumber != '')
             PassyPadding(RecordButton(
               title: localizations.idNumber,
-              value: _idCard.idNumber,
+              value: _idCard!.idNumber,
             )),
-          if (_idCard.name != '')
+          if (_idCard!.name != '')
             PassyPadding(RecordButton(
               title: localizations.name,
-              value: _idCard.name,
+              value: _idCard!.name,
             )),
-          if (_idCard.country != '')
+          if (_idCard!.country != '')
             PassyPadding(RecordButton(
-                title: localizations.country, value: _idCard.country)),
-          if (_idCard.issDate != '')
+                title: localizations.country, value: _idCard!.country)),
+          if (_idCard!.issDate != '')
             PassyPadding(RecordButton(
-                title: localizations.dateOfIssue, value: _idCard.issDate)),
-          if (_idCard.expDate != '')
+                title: localizations.dateOfIssue, value: _idCard!.issDate)),
+          if (_idCard!.expDate != '')
             PassyPadding(RecordButton(
-                title: localizations.expirationDate, value: _idCard.expDate)),
-          for (CustomField _customField in _idCard.customFields)
+                title: localizations.expirationDate, value: _idCard!.expDate)),
+          for (CustomField _customField in _idCard!.customFields)
             PassyPadding(CustomFieldButton(customField: _customField)),
-          if (_idCard.additionalInfo != '')
+          if (_idCard!.additionalInfo != '')
             PassyPadding(RecordButton(
                 title: localizations.additionalInfo,
-                value: _idCard.additionalInfo)),
+                value: _idCard!.additionalInfo)),
         ],
       ),
     );
--- lib/screens/id_cards_screen.dart
+++ lib/screens/id_cards_screen.dart
@@ -23,72 +23,93 @@ class IDCardsScreen extends StatefulWidget {
 
 class _IDCardsScreen extends State<IDCardsScreen> {
   final LoadedAccount _account = data.loadedAccount!;
+  List<String> _tags = [];
 
   void _onAddPressed() =>
       Navigator.pushNamed(context, EditIDCardScreen.routeName);
 
-  void _onSearchPressed() {
-    Navigator.pushNamed(context, SearchScreen.routeName, arguments:
-        SearchScreenArgs(builder: (String terms, List<String> tags, void Function() rebuild) {
-      final List<IDCardMeta> _found = [];
-      final List<String> _terms = terms.trim().toLowerCase().split(' ');
-      for (IDCardMeta _idCard in _account.idCardsMetadata.values) {
-        {
-          bool testIDCard(IDCardMeta value) => _idCard.key == value.key;
+  void _onSearchPressed({String? tag}) {
+    Navigator.pushNamed(context, SearchScreen.routeName,
+        arguments: SearchScreenArgs(
+            notSelectedTags: _tags.toList()..remove(tag),
+            selectedTags: tag == null ? [] : [tag],
+            builder:
+                (String terms, List<String> tags, void Function() rebuild) {
+              final List<IDCardMeta> _found = [];
+              final List<String> _terms = terms.trim().toLowerCase().split(' ');
+              for (IDCardMeta _idCard in _account.idCardsMetadata.values) {
+                {
+                  bool testIDCard(IDCardMeta value) => _idCard.key == value.key;
 
-          if (_found.any(testIDCard)) continue;
-        }
-        {
-          int _positiveCount = 0;
-          for (String _term in _terms) {
-            if (_idCard.nickname.toLowerCase().contains(_term)) {
-              _positiveCount++;
-              continue;
-            }
-            if (_idCard.name.toLowerCase().contains(_term)) {
-              _positiveCount++;
-              continue;
-            }
-          }
-          if (_positiveCount == _terms.length) {
-            _found.add(_idCard);
-          }
-        }
-      }
-      if (_found.isEmpty) {
-        return CustomScrollView(
-          slivers: [
-            SliverFillRemaining(
-              hasScrollBody: false,
-              child: Column(
-                children: [
-                  const Spacer(flex: 7),
-                  Text(
-                    localizations.noSearchResults,
-                    textAlign: TextAlign.center,
-                  ),
-                  const Spacer(flex: 7),
-                ],
-              ),
-            ),
-          ],
-        );
-      }
-      return IDCardButtonListView(
-        idCards: _found,
-        shouldSort: true,
-        onPressed: (idCard) => Navigator.pushNamed(
-          context,
-          IDCardScreen.routeName,
-          arguments: _account.getIDCard(idCard.key),
-        ),
-        popupMenuItemBuilder: idCardPopupMenuBuilder,
-      );
-    }));
+                  if (_found.any(testIDCard)) continue;
+                }
+                {
+                  bool _tagMismatch = false;
+                  for (String tag in tags) {
+                    if (_idCard.tags.contains(tag)) continue;
+                    _tagMismatch = true;
+                  }
+                  if (_tagMismatch) continue;
+                  int _positiveCount = 0;
+                  for (String _term in _terms) {
+                    if (_idCard.nickname.toLowerCase().contains(_term)) {
+                      _positiveCount++;
+                      continue;
+                    }
+                    if (_idCard.name.toLowerCase().contains(_term)) {
+                      _positiveCount++;
+                      continue;
+                    }
+                  }
+                  if (_positiveCount == _terms.length) {
+                    _found.add(_idCard);
+                  }
+                }
+              }
+              if (_found.isEmpty) {
+                return CustomScrollView(
+                  slivers: [
+                    SliverFillRemaining(
+                      hasScrollBody: false,
+                      child: Column(
+                        children: [
+                          const Spacer(flex: 7),
+                          Text(
+                            localizations.noSearchResults,
+                            textAlign: TextAlign.center,
+                          ),
+                          const Spacer(flex: 7),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }
+              return IDCardButtonListView(
+                idCards: _found,
+                shouldSort: true,
+                onPressed: (idCard) => Navigator.pushNamed(
+                  context,
+                  IDCardScreen.routeName,
+                  arguments: _account.getIDCard(idCard.key),
+                ),
+                popupMenuItemBuilder: idCardPopupMenuBuilder,
+              );
+            }));
+  }
+
+  Future<void> _load() async {
+    List<String> newTags = await _account.idCardsTags;
+    if (mounted) {
+      setState(() {
+        _tags = newTags;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    _load();
     List<IDCardMeta> _idCards = _account.idCardsMetadata.values.toList();
     return Scaffold(
       appBar: EntriesScreenAppBar(
@@ -133,6 +154,20 @@ class _IDCardsScreen extends State<IDCardsScreen> {
                         context, EditIDCardScreen.routeName),
                   ),
                 ),
+                if (_tags.isNotEmpty)
+                  Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                          top: PassyTheme.passyPadding.top / 2,
+                          bottom: PassyTheme.passyPadding.bottom / 2),
+                      child: EntryTagList(
+                        notSelected: _tags,
+                        onAdded: (tag) => setState(() {
+                          _onSearchPressed(tag: tag);
+                        }),
+                      ),
+                    ),
+                  ),
               ],
               idCards: _idCards,
               shouldSort: true,
