752edb06275affe583a402778a55a01bdadcdaaa
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2024-03-17 14:38:17 +0000

    Update widgets.dart

--- lib/passy_flutter/widgets/widgets.dart
+++ lib/passy_flutter/widgets/widgets.dart
@@ -38,5 +38,6 @@ export 'payment_card_button.dart';
 export 'record_button.dart';
 export 'record_dialog.dart';
 export 'rename_file_dialog.dart';
+export 'rename_tag_dialog.dart';
 export 'string_generator_dialog.dart';
 export 'three_widget_button.dart';
