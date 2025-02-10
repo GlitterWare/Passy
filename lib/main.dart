588de56e0e8dda482c834116542a058303be74e6
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2025-02-10 13:54:42 +0000

    `url_strategy` replaced with `flutter_web_plugins`

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:passy_website/passy_flutter/passy_flutter.dart';
 import 'package:passy_website/screens/downloads_screen.dart';
-import 'package:url_strategy/url_strategy.dart';
 import 'screens/main_screen.dart';
 
 void main() {
-  setPathUrlStrategy();
+  usePathUrlStrategy();
   runApp(const MyApp());
 }
 
