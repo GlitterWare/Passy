f21a89620c95739d6b6a3fdbf4decfd06eb93f51
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2025-02-17 02:26:54 +0000

    ARM64 workflow updated

--- .github/workflows/arm64.sh
+++ .github/workflows/arm64.sh
@@ -14,7 +14,7 @@ echo "===================================================="
 
 mkdir /passy-build/cli
 mkdir /passy-build/Passy
-mkdir -p /passy-build/Passy-No-Updates-Popup/Passy
+mkdir /passy-build/Passy-No-Updates-Popup
 export PATH="$PATH:$PWD/submodules/flutter/bin"
 git config --global --add safe.directory /Passy
 git config --global --add safe.directory /Passy/submodules/flutter
@@ -53,6 +53,6 @@ echo "Build Passy No Updates Popup"
 echo "===================================================="
 
 flutter build linux --dart-define=UPDATES_POPUP_ENABLED=false
-cp -r /Passy/build/linux/arm64/release/bundle/. /passy-build/Passy-No-Updates-Popup/Passy
+cp -r /Passy/build/linux/arm64/release/bundle/. /passy-build/Passy-No-Updates-Popup
 
 echo "All Done!"
