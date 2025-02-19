7bc1ea71db6599f70756f21f47d832008b23c6c2
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2025-02-19 22:32:25 +0000

    Update download_releases.sh

--- download_releases.sh
+++ download_releases.sh
@@ -30,31 +30,35 @@ cd ./build/gh_releases
 echo 'Downloading latest release artifacts...'
 gh run download \
   -n android \
+  -n cli-linux \
+  -n cli-linux-arm64 \
+  -n cli-linux-armv7 \
   -n linux-appimage \
+  -n linux-arm64-appimage \
+  -n linux-arm64-bundle \
+  -n linux-arm64-bundle-no-updates-popup \
   -n linux-bundle \
   -n linux-bundle-no-updates-popup \
   -n linux-snap \
   -n windows-exe-installer \
-  -n windows-portable \
-  -n passy-cli-linux-arm64 \
-  -n passy-cli-linux-armv7 \
-  -n passy-linux-arm64 \
-  -n passy-linux-arm64-no-updates-popup
+  -n windows-portable
 echo 'Done.'
 
 echo 'Preparing Passy Snap...'
 cp linux-snap/* .
+echo 'Preparing Passy CLI Linux...'
+cp cli-linux/Passy-CLI-Linux.zip ./Passy-CLI-v$version-Linux.zip
 echo 'Preparing Passy CLI Linux ARM64...'
-cp passy-cli-linux-arm64/Passy-CLI-Linux-ARM64.zip ./Passy-CLI-v$version-Linux-ARM64.zip
+cp cli-linux-arm64/Passy-CLI-Linux-ARM64.zip ./Passy-CLI-v$version-Linux-ARM64.zip
 echo 'Preparing Passy CLI Linux ARMv7...'
-cp passy-cli-linux-armv7/Passy-CLI-Linux-ARMv7.zip ./Passy-CLI-v$version-Linux-ARMv7.zip
+cp cli-linux-armv7/Passy-CLI-Linux-ARMv7.zip ./Passy-CLI-v$version-Linux-ARMv7.zip
 echo 'Preparing Passy Android Apk...'
 cp android/Passy-Android.apk ./Passy-v$version.apk
 zip -9 ./Passy-v$version-Android-Apk.zip Passy-v$version.apk 
 rm Passy-v$version.apk
 echo 'Preparing Passy Linux ARM64...'
-cp passy-linux-arm64/Passy-Linux-ARM64.zip ./Passy-v$version-Linux-ARM64-Bundle.zip
-cp passy-linux-arm64-no-updates-popup/Passy-Linux-ARM64-No-Updates-Popup.zip ./Passy-v$version-Linux-ARM64-Bundle-No-Updates-Popup.zip
+cp linux-arm64-bundle/Passy-Linux-ARM64-Bundle.zip ./Passy-v$version-Linux-ARM64-Bundle.zip
+cp linux-arm64-bundle-no-updates-popup/Passy-Linux-ARM64-Bundle.zip ./Passy-v$version-Linux-ARM64-Bundle-No-Updates-Popup.zip
 echo 'Preparing Passy Linux Bundle...'
 cp linux-bundle/Passy-Linux-Bundle.zip ./Passy-v$version-Linux-Bundle.zip
 cp linux-bundle-no-updates-popup/Passy-Linux-Bundle.zip ./Passy-v$version-Linux-Bundle-No-Updates-Popup.zip
@@ -70,6 +74,12 @@ unzip Passy-Linux-AppImage.zip
 rm Passy-Linux-AppImage.zip
 chmod +x Passy-Latest-x86_64.AppImage
 mv Passy-Latest-x86_64.AppImage Passy-v$version-x86-64.AppImage
+echo 'Preparing Passy Linux ARM64 AppImage...'
+cp linux-arm64-appimage/Passy-Linux-ARM64-AppImage.zip .
+unzip Passy-Linux-ARM64-AppImage.zip
+rm Passy-Linux-ARM64-AppImage.zip
+chmod +x Passy-Latest-aarch64.AppImage
+mv Passy-Latest-aarch64.AppImage Passy-v$version-aarch64.AppImage
 
 echo 'All done.'
 echo "Passy releases: $PWD/build/gh_releases"
