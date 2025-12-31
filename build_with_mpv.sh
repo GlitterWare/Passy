30982c90e46cacb695eb6284cfc28c50cb4bc10f
GleammerRay <101527589+GleammerRay@users.noreply.github.com>
2025-12-31 23:29:16 +0000

    Hermetic MPV build pipeline implemented
    Allows building Passy with fully hermetic MPV dependencies included under `bundle/lib` to allow the use of media_kit in bundle and AppImage builds on previously unsupported systems.

---
+++ build_with_mpv.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+cd "$(dirname "$0")"
+set -euo pipefail
+
+PASSY_LIB="$(echo $(pwd)/build/linux/*/release/bundle/lib)"
+MPV_BUILD="$(pwd)/build/mpv"
+MPV_PREFIX="$MPV_BUILD/prefix"
+MPV_LIB="$MPV_PREFIX/lib"
+MPV_INCLUDE="$MPV_PREFIX/include"
+MPV_PKGCONFIG="$MPV_LIB/pkgconfig"
+
+export PKG_CONFIG_PATH="$MPV_PKGCONFIG"
+export LD_LIBRARY_PATH="$MPV_LIB"
+export LIBRARY_PATH="$MPV_LIB"
+export C_INCLUDE_PATH="$MPV_INCLUDE"
+export CPLUS_INCLUDE_PATH="$MPV_INCLUDE"
+export CFLAGS="-I$MPV_INCLUDE"
+export CXXFLAGS="-I$MPV_INCLUDE"
+export LDFLAGS="-L$MPV_LIB"
+
+echo "Building hermetic MPV..."
+cmake -S "linux/mpv" -B "$MPV_BUILD" --log-level=ERROR
+cmake --build "$MPV_BUILD"
+
+echo "Building Passy with hermetic MPV..."
+flutter build linux --no-version-check --suppress-analytics $@
+
+MPV_TEMP="$(mktemp -d)"
+cp -r -P "$MPV_LIB"/pulseaudio/*.so* "$MPV_TEMP"
+cp -r -P "$MPV_LIB"/*.so* "$MPV_TEMP"
+
+echo "Patching MPV rpath..."
+patchelf --set-rpath '$ORIGIN' "$MPV_TEMP"/*.so*
+
+echo "Installing MPV libraries into Passy bundle..."
+cp -r -P "$MPV_TEMP"/*.so* "$PASSY_LIB"
+rm -rf "$MPV_TEMP"
+
+echo "Patching media_kit rpath..."
+patchelf --set-rpath '$ORIGIN' "$PASSY_LIB"/libmedia_kit*.so
+
+echo "Build complete."
